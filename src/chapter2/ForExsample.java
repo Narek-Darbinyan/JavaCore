9196c1efd40693aecd24fe1101785ee368c3f165
ACER <darbinyan.199229@gmail.com>
2022-12-16 22:15:53 +0400

    add some example

---
+++ src/chapter2/ForExsample.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class ForExsample {
+        public static void main(String[] args) {
+
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j<=i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+
+
+        }
+
+    }
