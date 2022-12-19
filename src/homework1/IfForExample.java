d158312c66da21db487e27ae8a5b1a4c53c60390
ACER <darbinyan.199229@gmail.com>
2022-12-20 00:36:39 +0400

    add some example

--- src/homework1/IfForExample.java
+++ src/homework1/IfForExample.java
@@ -4,12 +4,13 @@ public class IfForExample {
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
-        for (int i = 5; a <=b ; a++) {
-            System.out.println(a);
+        for (int i = a; i <=b ; i++) {
+            System.out.println(i);
         }
     }
 
 
 }
+
 
 
