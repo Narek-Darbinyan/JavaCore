5e85cafa5df6d3946dabf3e8c7b11a854ce0cb9a
ACER <darbinyan.199229@gmail.com>
2022-12-21 01:54:27 +0400

    add some example

--- src/homework1/IfForExample1.java
+++ src/homework1/IfForExample1.java
@@ -2,10 +2,12 @@ package homework1;
 
 public class IfForExample1 {
     public static void main(String[] args) {
-        int a=5;
-        int b=10;
-        for (int i = a; i < b; i++) {
-            System.out.print(a++);
-        }
+        char a = 88;
+        char b = 42;
+        System.out.println(a);
+        System.out.println(b);
+
+
     }
 }
+
---
+++ src/homework1/IfForExample2.java
@@ -0,0 +1,15 @@
+package homework1;
+
+public class IfForExample2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 40;
+        if (a<b);
+        System.out.println();
+
+    }
+}
+
+
+
+
